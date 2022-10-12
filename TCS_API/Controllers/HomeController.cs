7d7c07e6d5917d5542f9ce1a479747b510ad7195
Siddharth Dwivedi <siddharthdwivedi318@gmail.com>
2022-10-12 14:41:57 +0530

    Add project files.

---
+++ TCS_API/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TCS_API.Models;
+
+namespace TCS_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+
+
+        //This action will help in fetching the list of Hotels.
+
+
+        [HttpGet]
+        public List<Hotel> GetHotels()
+        {
+            List<Hotel> hotels = new List<Hotel>();
+            try
+            {
+                
+                hotels.Add(new Hotel() { HotelID = 1, HotelName = "Taj", CostPerDay = 20000 });
+                hotels.Add(new Hotel() { HotelID = 2, HotelName = "Milan", CostPerDay = 21000 });
+                hotels.Add(new Hotel() { HotelID = 3, HotelName = "Stay Inn", CostPerDay = 15000 });
+
+            }
+            catch (Exception)
+            {
+
+                hotels = null;
+            }
+
+            return hotels;
+
+        }
+
+
+    }
+}
---
+++ TCS_API/Models/Hotel.cs
@@ -0,0 +1,12 @@
+﻿namespace TCS_API.Models
+{
+    public class Hotel
+    {
+        public int HotelID { get; set; }
+        public string HotelName { get; set; }
+        public double CostPerDay { get; set; }
+
+
+
+    }
+}
